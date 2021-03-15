4a9fe82f9b751504b35784c71fc948c223136722
Maxwell Webb <60947369+maxwebbnz@users.noreply.github.com>
2021-03-16 12:27:56 +1300

    Added in HTML components of scoreboard, not yet done but in progress on being worked on

--- firebase/fb_leaderboard.js
+++ firebase/fb_leaderboard.js
@@ -10,9 +10,10 @@
  *========================================================================**/
 
 // Base object
-let leaderboardName;
-let leaderboardScore;
-let leaderboard = {
+
+let leaderBoardArray = [];
+
+let leaderBoard = {
     /**========================================================================
      **                           Return Leaderboard Data
      *?  Finds the data in the database, and returns it to an object
@@ -20,20 +21,50 @@ let leaderboard = {
      *@param name type  
      *@return n/a
      *========================================================================**/
-    init: function() {
-        var leaderboard = firebase.database().ref('scoreboard/')
-        leaderboard.ref.once('value', (_data) => {
-            _data.forEach((_childSnapshot) => {
-                var content = '';
-                leaderboardName = _childSnapshot.key;
-                leaderboardScore = _childSnapshot.val();
-                content += '<tr>';
-                content += '<td>' + leaderboardName + '</td>';
-                content += '<td>' + leaderboardScore + '</td>';
-                content += '</tr>';
-                $('#scoreBoard').append(content);
-                console.log("leaderboard.init() | Appended and retrieved scoreboard.")
+    init: function(_levelNum) {
+        let scores = []
+        var leaderboard = firebase.database().ref("scoreBoard/level" + _levelNum).orderByChild("hits").limitToLast(100);;
+        leaderboard.once('value').then(
+            (_snapshot) => {
+                _snapshot.forEach(function(childSnapshot) {
+                    console.log(childSnapshot.key)
+                    scores.unshift(childSnapshot.child("hits"))
+                    console.log(childSnapshot.child("hits").val())
+                    let userObject = {
+                        "name": childSnapshot.child("name").val(),
+                        "avatar": childSnapshot.child("avatar").val(),
+                        "hits": childSnapshot.child('hits').val(),
+                        "misses": childSnapshot.child('misses').val(),
+                        "userToken": childSnapshot.key
+                    }
+                    leaderBoardArray.push(userObject)
+                    console.log(leaderBoardArray)
+                })
+                for (i = leaderBoardArray.length; i--;) {
+                    $("tbody#scoreBoard tr").remove()
+                    let userAverage = leaderBoardArray[i].hits / leaderBoardArray[i].misses
+                    this.appendTable(leaderBoardArray[i].avatar, leaderBoardArray[i].name, leaderBoardArray[i].hits, leaderBoardArray[i].misses, userAverage)
+                    console.log(leaderBoardArray[i].name)
+                }
             });
-        });
     },
+    appendTable: function(_userAvatar, _userName, _userHits, _userMisses, _userAverage) {
+        var content = '';
+        content += '<tr>';
+        content += '<td><img src="' + _userAvatar + '" class="rounded-circle" width="20" height="20"></td>';
+        content += '<td>' + _userName + '</td>';
+        content += '<td>' + _userHits + '</td>';
+        content += '<td>' + _userMisses + '</td>';
+        content += '<td>' + _userAverage + '</td>';
+        content += '</tr>';
+        $('#scoreBoardTable').append(content);
+    },
+    handler: function(_lvlnum, _method) {
+        document.getElementById("leaderBoardHTML").style.display = "block";
+        this.init(_lvlnum);
+        if (_method == "close") {
+            document.getElementById("leaderBoardHTML").style.display = "none";
+
+        }
+    }
 }
