04f9a2d426f7de35f9dcaf5e0b3239e73857fb24
Maxwell Webb <60947369+maxwebbnz@users.noreply.github.com>
2021-03-29 21:25:28 +1300

    commit to beta

---
+++ ui/ui_admin.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 Max Webb
+ * All rights reserved.
+ */
+/**========================================================================
+ *                           Admin Module
+ *========================================================================**/
+
+
+let adminUI = {
+    handler: function(_action) {
+        if (_action == "show") {
+            console.log("Admin Admin Admin Admin on the wall")
+        }
+    }
+}
